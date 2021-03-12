25e18e23b25e27f5122406b7e57e3cacbb2c0c37
simamzadeh <sima.mz246@gmail.com>
2021-03-12 16:05:47 +0000

    Create bounce
    Create loop for ball to bounce when it hits surface

--- ball_bouncing.py
+++ ball_bouncing.py
@@ -1,10 +1,18 @@
 from vpython import *
 
-sphere(pos=vector(0,10,0), color = color.red)
+ball = sphere(pos=vector(0,10,0), color = color.red)
 
-box(pos=vector(0,0,0), color=color.green, size=vector(10, 0.5, 10))
+surface = box(pos=vector(0,0,0), color=color.green, size=vector(10, 0.5, 10))
 
 # box.color = color.yellow # changing the color after assignment
 # box.pos gives the position of the box
 # box.pos.x can change the specific coordinate x
 
+n = 0
+change = -0.1
+while n < 200:
+    rate(100)
+    n += 1
+    ball.pos.y = ball.pos.y + change
+    if ball.pos.y < 0:
+        change = -change
