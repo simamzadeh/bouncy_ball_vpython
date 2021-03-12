5160de4e3db94bcca2812151a35a947b0a910dfe
simamzadeh <sima.mz246@gmail.com>
2021-03-12 15:44:38 +0000

    Add sphere and surface
    Add sphere (aka the ball) and surface at starting position

--- ball_bouncing.py
+++ ball_bouncing.py
@@ -1,1 +1,10 @@
 from vpython import *
+
+sphere(pos=vector(0,10,0), color = color.red)
+
+box(pos=vector(0,0,0), color=color.green, size=vector(10, 0.5, 10))
+
+# box.color = color.yellow # changing the color after assignment
+# box.pos gives the position of the box
+# box.pos.x can change the specific coordinate x
+
